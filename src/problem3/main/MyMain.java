8a39f0daf5dba9bf9be19308349fcf5cd1e1674f
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-27 21:07:19 +0530

    main class to execute pogram

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+        myPriorityQueue.enquue(5);
+        myPriorityQueue.enquue(10);
+        myPriorityQueue.enquue(2);
+        myPriorityQueue.sort(myPriorityQueue.getRear());
+        System.out.println("Queue" + myPriorityQueue.toString());
+
+    }
 }
