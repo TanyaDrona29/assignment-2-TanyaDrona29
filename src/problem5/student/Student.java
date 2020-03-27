8808d7899efce71e15fc1caa05a34aaed14b7a5a
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-03-27 22:24:25 +0530

    Student class defination

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -5,6 +5,23 @@
  *  Time: 7:06 PM
  */
 package problem5.student;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // to store student information and properties
 public class Student {
+    ArrayList arrayList = new ArrayList();
+
+    void add() {
+        {
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+            for (int i = 0; i < arrayList.size(); i++) {
+                arrayList.add(a);
+            }
+
+
+        }
+    }
 }
